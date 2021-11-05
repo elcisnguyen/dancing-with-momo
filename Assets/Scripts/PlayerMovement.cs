678402352fb9291c4b06db124dddba23e908ae97
Nguyễn Tấn Hào <tanhaonguyen2015@gmail.com>
2021-11-05 22:55:28 +0700

    Make the ball bounces on tiles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,29 +2,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public Rigidbody rigidBody;
+    public GameObject[] tiles;
+
+    public float forwardForce;
+    public float upwardForce;
+    public float sidewaysForce;
   
+    void Start()
+    {
+        //Update ham sort sau
+        //tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+
+        //for (int i = 0; i < tiles.Length; i++)
+        //    Debug.Log(tiles[i].transform.position);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Add a forward force
-        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rigidBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rigidBody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         //Condition to restart the level
         //if (rb.position.y < 0f)
         //{
         //    FindObjectOfType<GameManager>().EndGame();
         //}
+       
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.collider.tag == "Obstacle")
+        //{
+        //    Debug.Log("We hit obstacle");
+        //    playerMovement.enabled = false; //Make player stop moving when collision happens
+        //    FindObjectOfType<GameManager>().EndGame();
+        //}
+        Debug.Log("COLLISION!");
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(0, forwardForce, upwardForce, ForceMode.VelocityChange);
+        Debug.Log(rigidBody.position);
     }
 }
